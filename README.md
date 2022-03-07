2b3647504765f72ceabc3880c1c3930269cddf7f
devNhaan <nhandeptrai137@gmail.com>
2022-03-08 00:13:17 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# java16-gira-nhan
+ Cybersoft Education - Java backend - java16
---
+++ src/main/java/cybersoft/javabackend/java16giranhan/Java16GiraNhanApplication.java
@@ -0,0 +1,13 @@
+package cybersoft.javabackend.java16giranhan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java16GiraNhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java16GiraNhanApplication.class, args);
+	}
+
+}
