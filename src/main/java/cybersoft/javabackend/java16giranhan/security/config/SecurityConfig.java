559abce0ecb29b07738b8882f69e09909785b0bc
devNhaan <nhandeptrai137@gmail.com>
2022-04-22 23:35:50 +0700

    add security module

---
+++ src/main/java/cybersoft/javabackend/java16giranhan/security/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package cybersoft.javabackend.java16giranhan.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Autowired
+	private UserDetailsService userDetailsService;
+	@Bean 
+	public PasswordEncoder getPasswordEncoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService)
+			.passwordEncoder(getPasswordEncoder());
+	}
+	
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		//CORS
+		http.cors();
+		
+		//SESSTION -> STATELESS
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		//CSRF
+		http.csrf();
+		
+		//jwt filter
+		
+		// API AUTHENTICATION
+		http.antMatcher("/api/v1/**").authorizeRequests()
+									.anyRequest().authenticated();
+									
+		
+		
+	}
+	
+}
